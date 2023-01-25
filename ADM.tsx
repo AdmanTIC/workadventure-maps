3e0d49959c144d20b7bddfdac2d9d72d7ff7544b
Clement Maroy <cmaroy@admantic.fr>
2023-01-25 17:16:02 +0100

    added collision, lunch table, ADM logo, minor changes

--- ADM.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="ADM" tilewidth="3289" tileheight="1322" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="3289" height="1322" source="tilesets/admantic-logo-carre-v3-fond-blanc copie.jpg"/>
- </tile>
-</tileset>
