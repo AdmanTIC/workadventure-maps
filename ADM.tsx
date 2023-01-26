6b10ef528909f37b3b7f4cb7971891f97d490e78
Clement Maroy <cmaroy@admantic.fr>
2023-01-26 12:15:04 +0100

    object layer added

---
+++ ADM.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="ADM" tilewidth="1517" tileheight="673" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1517" height="673" source="tilesets/admantic-logo-rect_Fond-blanc.png"/>
+ </tile>
+</tileset>
